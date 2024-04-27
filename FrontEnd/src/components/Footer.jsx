29c9c592ff562e81ff707b12dcea8200de6d0c4a
Prateek195jain <118386573+Prateek195jain@users.noreply.github.com>
2024-04-27 15:27:19 +0530

    Update Footer.jsx

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="bg-slate-800 px-10 py-10 text-white text-center">
-      <h5>Made by Ashutosh</h5>
+      <h5>Made by Prateek</h5>
     </div>
   );
 }
